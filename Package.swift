6ec45f7fc61a04ebba02265293356c655627814e
Robert Geifman <robertgeifman@me.com>
2020-08-04 11:45:18 +0200

    removed documentation, upgradefd package info
    Signed-off-by: Robert Geifman <robertgeifman@me.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwipeCellKit",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
